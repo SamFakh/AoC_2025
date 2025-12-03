887dd1eff2beb097d07ff059cf6439884d746938
Sam Fakhraee <Sam.Fakhraee@nmbu.no>
2025-12-03 01:52:12 +0100

    docs

---
+++ mad-mercury/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://samfredrik.github.io/AoC_2025",
+  base: "/AoC_2025/",
+  outDir: "../docs",
+});
